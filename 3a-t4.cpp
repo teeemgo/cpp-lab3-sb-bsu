19986f30e63e8d8ba7a5491a85a3c63eb7eb2c29
teeemgo <146633035+teeemgo@users.noreply.github.com>
2023-11-18 18:13:27 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,33 +1,16 @@
-#include <iostream> 
+#include<iostream> 
 using namespace std; 
-int main()
 
+int main()
 {
-int number = 0;
-cout << "Enter a number from 1 to 5 : "; 
-cin >> number; //initialize number
-switch(number) 
-/* contains expression we check. Can be an integer or character */
-{ // starting switch code block
-/* when matched with case, the statements below are executed until the end of switch block or until break statement is met */
-case 1:
-cout << "one";
-break;
-case 2:
-cout << "two"; 
-break;
-case 3:
-cout << "three"; 
-break;
-case 4:
-cout << "four"; 
-break;
-case 5:
-cout << "five"; 
-break;
-/* default statements are executed when no other conditions are met */
-default :
-cout << "The number is not within 1 to 5 range";
-}
+int k, i = 1, sum_k = 0, number;
+cout << " Enter the amount of integers to sum = "; cin >> k;
+do {
+cout << "Enter integer nr. " << i << ": "; 
+cin >> number;
+sum_k += number; i++;
+} while (i <= k);
+cout << " The total sum of " << k << " integers is: "  << sum_k;
 return 0;
 }
+
